9f306f27c480977868aebe0f8fc600940f0fcec5
jiffyin7 <jiffyin7@gmail.com>
2024-10-16 15:52:07 +0900

    feat(InputValidator): Console API 라이브러리 추가
    이슈: #13

---
+++ src/main/java/calculator/stringAdder/inputvalidator/InputValidator.java
@@ -0,0 +1,16 @@
+package calculator.stringAdder.inputvalidator;
+
+import camp.nextstep.edu.missionutils.Console;
+
+/**
+ * @author : jiffyin7@gmail.com
+ * @since : 24. 10. 16.
+ */
+public class InputValidator {
+
+  public String validateInput() {
+    System.out.println("덧셈할 문자열을 입력해주세요.");
+    String input = Console.readLine();
+    return null;
+  }
+}
